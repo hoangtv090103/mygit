54e46ed3d15f84531de969b3617f13c84dd8b530
hoangtv090103 <hoangtv090103@gmail.com>
2025-04-07 15:10:21 +0700

    hash-object: Save object

--- .mygit/data.py
+++ .mygit/data.py
@@ -1,5 +1,16 @@
 import os
+import hashlib
+
 GIT_DIR = '.mygit'
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+    
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
+    
+    
