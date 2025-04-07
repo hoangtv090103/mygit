5f2a14feb310b334fe6ddb79645eaec7e9426f5b
hoangtv090103 <hoangtv090103@gmail.com>
2025-04-07 17:30:17 +0700

    data: add types to objects

--- mygit/cli.py
+++ mygit/cli.py
@@ -30,11 +30,12 @@ def init(args):
     data.init()
     print(f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
 
+
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
-        
+
+
 def cat_file(args):
     sys.stdout.flush()
-    sys.stdout.buffer.write(data.get_object(args.object))
-    
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
