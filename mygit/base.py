ca1d2d427902c53ec126ece7615ed1b0942a49fd
hoangtv090103 <hoangtv090103@gmail.com>
2025-04-08 11:47:20 +0700

    write-tree: Write tree objects

--- mygit/base.py
+++ mygit/base.py
@@ -8,17 +8,29 @@ def write_tree(directory='.'):
     Write a tree object from the contents of the given directory.
     @param directory: The directory to write the tree from.
     """
+    entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
             if is_ignored(full):
                 continue
+
+            type_ = None
+            oid = None
+
             if entry.is_file(follow_symlinks=False):
+                type_ = 'blob'
                 with open(full, 'rb') as f:
-                    print(data.hash_object(f.read()), full)
-                print(full)
+                    oid = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+                type_ = 'tree'
+                oid = write_tree(full)
+
+            if type_ is not None and oid is not None:
+                entries.append((entry.name, oid, type_))
+
+    tree = ''.join(f'{type_} {oid} {name}\n' for name, oid, type_ in sorted(entries))
+    return data.hash_object(tree.encode(), 'tree')
 
 
 def is_ignored(path):
