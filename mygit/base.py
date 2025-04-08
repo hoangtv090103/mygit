e46b865fbb3a8b84d1995d7ba20b7cadbd7cb863
hoangtv090103 <hoangtv090103@gmail.com>
2025-04-08 11:34:49 +0700

    write-tree: ignore .mygit files

--- mygit/base.py
+++ mygit/base.py
@@ -11,9 +11,14 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored(ful):
+                continue
             if entry.is_file(follow_symlinks=False):
                 # TODO: write the file to object store
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
     
+    
+    def is_ignored(path):
+        return '.mygit' in path.split('/')
